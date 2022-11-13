091bcb79b131bc03efcc68d1ce83a01cbe143783
Leo Terrier <leoterrier22@gmail.com>
2022-11-13 18:44:03 +0100

    second commit

--- public/script.js
+++ public/script.js
@@ -1,17 +1,12 @@
 const sections = document.querySelectorAll('.section')
 const navLinks = document.querySelectorAll('nav a')
 sections.forEach(elt => {
-  elt.addEventListener('mouseover', (e) => {
-    console.log(e.target.id)
+  elt.addEventListener("mouseover", () => {
     document.querySelector(`[href="#${elt.id}"]`).style.color = "var(--yellow)";
-
-
-  })
-  elt.addEventListener('mouseleave', (e) => {
-    console.log(e.target.id)
+  });
+  elt.addEventListener("mouseleave", () => {
     document.querySelector(`[href="#${elt.id}"]`).style.color = "white";
-    document.querySelector(`[href="#${elt.id}"]`).style.textShadow = "none";
-  })
+  });
 });
 
 navLinks.forEach(elt => {
@@ -24,3 +19,23 @@ navLinks.forEach(elt => {
   });
 })
 
+const githubLogo = document.querySelectorAll("#githubLogo")
+githubLogo.forEach(elt => {
+  elt.addEventListener("mouseover", () => {
+    elt.src = "./stackLogo/github.png"
+  })
+  elt.addEventListener("mouseleave", () => {
+    elt.src = "./stackLogo/githubW.png"
+  })
+});
+/* const gifTexts = document.querySelectorAll(".gifText");
+gifTexts.forEach((elt) => {
+  elt.addEventListener("mouseover", () => {
+    console.log("hey");
+    elt.style.display = "none";
+  });
+  elt.addEventListener("mouseleave", () => {
+    console.log("ho");
+    elt.style.display = "block";
+  });
+}); */
